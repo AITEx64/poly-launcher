8b9058ebd01f77d33bba0410bdaa9b35909254cd
ripSquid <44409107+ripSquid@users.noreply.github.com>
2021-11-20 12:56:45 +0100

    Not the rust part actually compiles and probably somewhat works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,43 +3,59 @@
   windows_subsystem = "windows"
 )]
 
-use std::io::BufWriter;
-use tauri::Manager;
-use steamworks;
-use image::{self, RgbaImage};
 use base64;
+use image::{self, RgbaImage};
+use std::{io::BufWriter, sync::Arc};
+use steamworks;
+use tauri::{App, AppHandle, Manager};
 
 #[derive(serde::Serialize, Clone)]
 struct Payload {
   name: String,
-  avatar: String
+  avatar: String,
 }
-
 
 fn main() {
   let (client, _single) = steamworks::Client::init().unwrap();
-
   let myself = client.friends().get_friend(client.user().steam_id());
-
-
-
+  let name = myself.name();
+  let base64_or_none_pic = match myself.small_avatar() {
+    Some(buffer) => {
+      let mut avatar_png: Vec<u8> = vec![];
+      image::codecs::png::PngEncoder::new(&mut avatar_png).encode(
+        &buffer,
+        32,
+        32,
+        image::ColorType::Rgba8,
+      );
+      base64::encode(avatar_png)
+    }
+    None => String::from("none"),
+  };
   tauri::Builder::default()
-    .setup(|app| {
-      let steamworks_name = app.listen_global("get_steam", |_event| {
-        let base64_or_none_pic = match myself.small_avatar() {
-          Some(buffer) => {
-            let avatar_png: Vec<u8> = vec!();
-            image::codecs::png::PngEncoder::new(avatar_png).encode(&buffer, 32, 32, image::ColorType::Rgba8);
-            base64::encode(avatar_png)
-          },
-          None => {
-            String::from("none")
-          }
-        };
-        app.emit_all("steam_data", Payload {name: myself.name(), avatar: base64_or_none_pic});
+    .setup(move |app| {
+      let apphandle = app.handle();
+      let steam_name = name.clone();
+      let steam_avatar = base64_or_none_pic.clone();
+      let steamworks_name = app.listen_global("get_steam", move |_event| {
+        emit_steam_data(&apphandle, &steam_name, &steam_avatar);
       });
+      
+
+      
+
       Ok(())
     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+fn emit_steam_data(app: &AppHandle, name: &str, avatar: &str) {
+  app.emit_all(
+    "steam_data",
+    Payload {
+      name: String::from(name),
+      avatar: String::from(avatar),
+    },
+  );
 }
